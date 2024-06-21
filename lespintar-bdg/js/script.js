07fdb4f7b5565ef1f472d23a342bb5e2394cdee8
Dicky Sidiek <63240809+dickyms@users.noreply.github.com>
2024-06-21 08:39:54 +0700

    Delete lespintar-bdg directory

--- lespintar-bdg/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('html/header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('snippet-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error fetching the HTML snippet:', error));
-});
